d6cd9c855a26494eb5c62bf401d2d8e37aac902d
Xavier Fischer <xfischer@cityway.fr>
2020-01-28 15:34:18 +0100

    intervisibility progress

--- DEMNet.Sample/Samples/IntervisibilitySample.cs
+++ DEMNet.Sample/Samples/IntervisibilitySample.cs
@@ -1,58 +1,58 @@
-﻿    //
-    // IntervisibilitySample.cs
-    //
-    // Author:
-    //       Xavier Fischer
-    //
-    // Copyright (c) 2019 
-    //
-    // Permission is hereby granted, free of charge, to any person obtaining a copy
-    // of this software and associated documentation files (the "Software"), to deal
-    // in the Software without restriction, including without limitation the rights
-    // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    // copies of the Software, and to permit persons to whom the Software is
-    // furnished to do so, subject to the following conditions:
-    //
-    // The above copyright notice and this permission notice shall be included in
-    // all copies or substantial portions of the Software.
-    //
-    // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    // AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-    // THE SOFTWARE.
+﻿//
+// IntervisibilitySample.cs
+//
+// Author:
+//       Xavier Fischer
+//
+// Copyright (c) 2019 
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
-    using DEM.Net.Core;
-    using GeoJSON.Net.Feature;
-    using GeoJSON.Net.Geometry;
-    using Microsoft.Extensions.Logging;
-    using Newtonsoft.Json;
-    using System;
-    using System.Collections.Generic;
-    using System.Diagnostics;
-    using System.Linq;
-    using System.Threading;
-    using System.Threading.Tasks;
+using DEM.Net.Core;
+using GeoJSON.Net.Feature;
+using GeoJSON.Net.Geometry;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
-    namespace SampleApp
+namespace SampleApp
+{
+    public class IntervisibilitySample
     {
-        public class IntervisibilitySample
-        {
-            private readonly ILogger<ElevationSamples> _logger;
-            private readonly IElevationService _elevationService;
+        private readonly ILogger<ElevationSamples> _logger;
+        private readonly IElevationService _elevationService;
 
-            public IntervisibilitySample(ILogger<ElevationSamples> logger
-                    , IElevationService elevationService)
+        public IntervisibilitySample(ILogger<ElevationSamples> logger
+                , IElevationService elevationService)
+        {
+            _logger = logger;
+            _elevationService = elevationService;
+        }
+        public void Run(CancellationToken cancellationToken)
+        {
+            try
             {
-                _logger = logger;
-                _elevationService = elevationService;
-            }
-            public void Run(CancellationToken cancellationToken)
-            {
-                try
-                {
 
                 // ventoux
                 //double lat1 = 44.17346;
@@ -63,61 +63,63 @@
                 //double lon1 = 5.612444;
 
                 // bottom ste victoire (fuveau)
-                //double lat1 = 43.479541;
-                //double lon1 = 5.552377;
+                double lat1 = 43.49029208393125;
+                double lon1 = 5.587234497070313;
+                //double lat2 = 43.53013134607789;
+                //double lon2 = 5.581398010253906;
 
                 // mont blanc
-                //double lat2 = 45.833;
-                //double lon2 = 6.864;
+                double lat2 = 45.833;
+                double lon2 = 6.864;
 
                 // bob
-                double lat1 = 37.212627;
-                double lon1 = 22.321612;
+                //double lat1 = 37.212627;
+                //double lon1 = 22.321612;
 
-                double lat2 = 37.208179;
-                double lon2 = 22.324373;
+                //double lat2 = 37.208179;
+                //double lon2 = 22.324373;
 
-                    Stopwatch sw = new Stopwatch();
-                
-                    if (cancellationToken.IsCancellationRequested) return;
-                    _logger.LogInformation("Line elevation");
+                Stopwatch sw = new Stopwatch();
 
-                    sw.Restart();
-                    // Line starting at mont ventoux peak to Mont Blanc
-                    var elevationLine = GeometryService.ParseGeoPointAsGeometryLine(new GeoPoint(lat1,lon1), new GeoPoint(lat2,lon2));
-                    DEMDataSet dataSet = DEMDataSet.ASTER_GDEMV3;
+                if (cancellationToken.IsCancellationRequested) return;
+                _logger.LogInformation("Line elevation");
 
-                    _elevationService.DownloadMissingFiles(dataSet, elevationLine.GetBoundingBox());
-                    var geoPoints = _elevationService.GetLineGeometryElevation(elevationLine, dataSet);
-                
-                    var metrics = geoPoints.ComputeVisibilityMetrics();
-                    _logger.LogInformation($"{dataSet.Name} metrics: {metrics.ToString()}");
+                sw.Restart();
+                // Line starting at mont ventoux peak to Mont Blanc
+                var elevationLine = GeometryService.ParseGeoPointAsGeometryLine(new GeoPoint(lat1, lon1), new GeoPoint(lat2, lon2));
+                DEMDataSet dataSet = DEMDataSet.ASTER_GDEMV3;
 
-                    //var geoJson = ConvertLineElevationResultToGeoJson(simplified);
-                
-                    _logger.LogInformation($"Done in {sw.Elapsed.TotalMilliseconds:N1}ms");
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogError(ex, ex.Message);
-                }
+                _elevationService.DownloadMissingFiles(dataSet, elevationLine.GetBoundingBox());
+                var geoPoints = _elevationService.GetLineGeometryElevation(elevationLine, dataSet);
+
+                var metrics = geoPoints.ComputeVisibilityMetrics();
+                _logger.LogInformation($"{dataSet.Name} metrics: {metrics.ToString()}");
+
+                //var geoJson = ConvertLineElevationResultToGeoJson(simplified);
+
+                _logger.LogInformation($"Done in {sw.Elapsed.TotalMilliseconds:N1}ms");
             }
-
-            private string ConvertLineElevationResultToGeoJson(List<GeoPoint> linePoints)
+            catch (Exception ex)
             {
-                FeatureCollection fc = new FeatureCollection(linePoints.Select(ConvertGeoPointToFeature).ToList());
-
-                return JsonConvert.SerializeObject(fc);
+                _logger.LogError(ex, ex.Message);
             }
-            private Feature ConvertGeoPointToFeature(GeoPoint point)
-            {
-                return new Feature(
-                        new Point(
-                                new Position(point.Latitude, point.Longitude, point.Elevation)
-                                )
-                        , point
-                        );
-            }
-
         }
+
+        private string ConvertLineElevationResultToGeoJson(List<GeoPoint> linePoints)
+        {
+            FeatureCollection fc = new FeatureCollection(linePoints.Select(ConvertGeoPointToFeature).ToList());
+
+            return JsonConvert.SerializeObject(fc);
+        }
+        private Feature ConvertGeoPointToFeature(GeoPoint point)
+        {
+            return new Feature(
+                    new Point(
+                            new Position(point.Latitude, point.Longitude, point.Elevation)
+                            )
+                    , point
+                    );
+        }
+
     }
+}
