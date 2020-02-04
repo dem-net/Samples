9d6a2b3b93d50463f625ef472e0b6de1ed74ee01
Xavier Fischer <xavier@fischer.ninja>
2020-02-04 23:54:08 +0100

    building samples logging

--- DEMNet.Sample/Samples/Extensions/OsmExtensionSample.cs
+++ DEMNet.Sample/Samples/Extensions/OsmExtensionSample.cs
@@ -25,16 +25,21 @@ namespace SampleApp
             bbox = new BoundingBox(5.444927726471018, 5.447502647125315, 43.52600685540608, 43.528138282848076);
             var triangulation = GetBuildings3D(bbox);
 
+            Task.Delay(1000).GetAwaiter().GetResult();
             // Aix en provence / slope
             bbox = new BoundingBox(5.434828019053151, 5.4601480721537365, 43.5386672180082, 43.55272718416761);
+            triangulation = GetBuildings3D(bbox);
 
+            Task.Delay(1000).GetAwaiter().GetResult();
             // POLYGON((5.526716197512567 43.56457608971906,5.6334895739774105 43.56457608971906,5.6334895739774105 43.49662332237486,5.526716197512567 43.49662332237486,5.526716197512567 43.56457608971906))
             // Aix en provence / ste victoire
             bbox = new BoundingBox(5.526716197512567, 5.6334895739774105, 43.49662332237486, 43.56457608971906);
+            triangulation = GetBuildings3D(bbox);
+
         }
 
 
-       
+
         private Triangulation GetBuildings3D(BoundingBox bbox)
         {
             try
@@ -43,9 +48,9 @@ namespace SampleApp
                     .WithWays("building")
                     .ToGeoJSON();
 
-                FeatureCollection buildings =  task.GetAwaiter().GetResult();
+                FeatureCollection buildings = task.GetAwaiter().GetResult();
 
-                
+
                 Triangulation triangulation = buildingService.Triangulate(buildings, DEMDataSet.AW3D30);
                 return triangulation;
             }
