95e03dd104202b41687d831b00a8fc74d2e22b13
xfischer <xavier@fischer.ninja>
2020-02-06 14:08:23 +0100

    osm sample 3D model first commit

--- DEMNet.Sample/Samples/Extensions/OsmExtensionSample.cs
+++ DEMNet.Sample/Samples/Extensions/OsmExtensionSample.cs
@@ -3,8 +3,10 @@ using DEM.Net.Extension.Osm;
 using DEM.Net.Extension.Osm.Buildings;
 using DEM.Net.Extension.Osm.OverpassAPI;
 using GeoJSON.Net.Feature;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -23,28 +25,33 @@ namespace SampleApp
             BoundingBox bbox;
             // Aix en provence / rotonde
             bbox = new BoundingBox(5.444927726471018, 5.447502647125315, 43.52600685540608, 43.528138282848076);
-            var triangulation = GetBuildings3D(bbox);
+            GetBuildings3D(bbox);
 
             Task.Delay(1000).GetAwaiter().GetResult();
             // Aix en provence / slope
             bbox = new BoundingBox(5.434828019053151, 5.4601480721537365, 43.5386672180082, 43.55272718416761);
-            triangulation = GetBuildings3D(bbox);
+            GetBuildings3D(bbox);
 
             Task.Delay(1000).GetAwaiter().GetResult();
             // POLYGON((5.526716197512567 43.56457608971906,5.6334895739774105 43.56457608971906,5.6334895739774105 43.49662332237486,5.526716197512567 43.49662332237486,5.526716197512567 43.56457608971906))
             // Aix en provence / ste victoire
             bbox = new BoundingBox(5.526716197512567, 5.6334895739774105, 43.49662332237486, 43.56457608971906);
-            triangulation = GetBuildings3D(bbox);
+            GetBuildings3D(bbox);
 
         }
 
 
 
-        private Triangulation GetBuildings3D(BoundingBox bbox)
+        private void GetBuildings3D(BoundingBox bbox, string modelName = "buildings")
         {
             try
             {
-                return buildingService.GetBuildings3D(bbox, DEMDataSet.ASTER_GDEMV3, false);
+                // debug: write geojson to file
+                //File.WriteAllText("buildings.json", JsonConvert.SerializeObject(buildingService.GetBuildingsGeoJson(bbox)));
+
+                var model = buildingService.GetBuildings3DModel(bbox, DEMDataSet.ASTER_GDEMV3, false);
+                model.SaveGLB(Path.Combine(Directory.GetCurrentDirectory(), modelName + ".glb"));
+
             }
             catch (Exception ex)
             {
