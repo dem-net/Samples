a92fb799a0a4d2d00c822ecca964f5842e1a8c32
Xavier Fischer <xavier@fischer.ninja>
2020-10-04 10:09:07 +0200

    litto3D sample

--- DEMNet.Sample/Samples/Core/CustomRasterElevationSample.cs
+++ DEMNet.Sample/Samples/Core/CustomRasterElevationSample.cs
@@ -26,9 +26,12 @@
 
 using DEM.Net.Core;
 using DEM.Net.Core.Datasets;
+using DEM.Net.Core.Imagery;
+using DEM.Net.glTF.SharpglTF;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 
 namespace SampleApp
@@ -41,40 +44,152 @@ namespace SampleApp
         private readonly ILogger<CustomRasterElevationSample> _logger;
         private readonly RasterService _rasterService;
         private readonly ElevationService _elevationService;
+        private readonly ImageryService _imageryService;
+        private readonly SharpGltfService _sharpGltfService;
 
         public CustomRasterElevationSample(ILogger<CustomRasterElevationSample> logger
                 , RasterService rasterService
-            , ElevationService elevationService)
+                , ElevationService elevationService
+                , SharpGltfService sharpGltfService
+                , ImageryService imageryService)
         {
             _logger = logger;
             _rasterService = rasterService;
             _elevationService = elevationService;
+            _sharpGltfService = sharpGltfService;
+            _imageryService = imageryService;
         }
         public void Run()
         {
             try
             {
-                var dataSetLitto3D = new DEMDataSet()
-                {
-                    Name = "Litto3D_PortCros",
-                    Description = "Litto3D_PortCros",
-                    PublicUrl = null,
-                    DataSource = new LocalFileSystem(localDirectory: @"C:\Repos\DEM.Net.Samples\DEMNet.Sample\SampleData\MNT1m PortCros"),
-                    FileFormat = new DEMFileDefinition("ASCIIGrid", DEMFileType.ASCIIGrid, ".asc", DEMFileRegistrationMode.Cell),
-                    ResolutionMeters = 1,
-                    SRID= Reprojection.SRID_PROJECTED_LAMBERT_93,
-                    Attribution = new Attribution("Dataset","Litto3D","www.shom.fr","Licence ouverte Etalab")
-                };
-                _rasterService.GenerateDirectoryMetadata(dataSetLitto3D, force: false);
+                bool withTexture = true;
+                int TEXTURE_TILES = 8; 
+                string outputDir = Directory.GetCurrentDirectory();
+                string modelName = "PortCros";
+                ImageryProvider imageryProvider = ImageryProvider.MapBoxSatellite;
 
-                var point = new GeoPoint(43.01142119356318, 6.385200681010872).ReprojectTo(4326, 2154);
-                point = _elevationService.GetPointElevation(point, dataSetLitto3D);
-              
+                //var datasetFiles = @"C:\Repos\DEM.Net.Samples\DEMNet.Sample\SampleData\MNT1m PortCros";
+                //DEMDataSet litto3DDataset = GetLitto3D_1Metre(datasetFiles, firstRun: true);
+                var datasetFiles = @"C:\Repos\DEM.Net.Samples\DEMNet.Sample\SampleData\MNT5m PortCros";
+                DEMDataSet litto3DDataset = GetLitto3D_5Metres(datasetFiles, firstRun: true);
+
+                _logger.LogInformation($"Getting height map data...");
+                // bbox total MNT 5m
+                //string bboxWkt = "POLYGON((6.379174977593318 43.01254201266237,6.382608205132381 43.01254201266237,6.382608205132381 43.00882338332376,6.379174977593318 43.00882338332376,6.379174977593318 43.01254201266237))";
+                string bboxWkt = "POLYGON((6.363825088730257 43.01654549682949,6.39970231651346 43.01654549682949,6.39970231651346 42.99112418454671,6.363825088730257 42.99112418454671,6.363825088730257 43.01654549682949))";
+                //string bboxWkt = "POLYGON((6.406395745104527 43.02231568794789,6.427166771715855 43.02231568794789,6.427166771715855 43.005339523906166,6.406395745104527 43.005339523906166,6.406395745104527 43.02231568794789))";
+                //string bboxWkt  = "POLYGON((6.412103485888219 43.01676208054169,6.428497147387242 43.01676208054169,6.428497147387242 43.004900152380664,6.412103485888219 43.004900152380664,6.412103485888219 43.01676208054169))";
+                ////string bboxWkt = "POLYGON((6.362806960421437 43.01537135435604,6.394821807223194 43.01537135435604,6.394821807223194 43.00112388903013,6.362806960421437 43.00112388903013,6.362806960421437 43.01537135435604))";
+                //string bboxWkt = "POLYGON((6.362852061542235 43.008752396912236,6.377357447894774 43.008752396912236,6.377357447894774 43.00206770457925,6.362852061542235 43.00206770457925,6.362852061542235 43.008752396912236))";
+                //string bboxWkt = "POLYGON((6.373237574847899 43.004667107932384,6.3788594849431135 43.004667107932384,6.3788594849431135 43.00127755196608,6.373237574847899 43.00127755196608,6.373237574847899 43.004667107932384))";
+                var bbox = GeometryService.GetBoundingBox(bboxWkt).ReprojectTo(4326,litto3DDataset.SRID);
+                var heightMap = _elevationService.GetHeightMap(ref bbox, litto3DDataset);
+                ModelGenerationTransform transform = new ModelGenerationTransform(bbox, 3857,centerOnOrigin:false, 2f, centerOnZOrigin: false);
+
+                var min = heightMap.Coordinates.Where(g =>( g.Elevation ?? 0d) > litto3DDataset.NoDataValue).Min(g=>g.Elevation ?? 0d);
+                _logger.LogInformation($"Processing height map data ({heightMap.Count} coordinates)...");
+                heightMap = transform.TransformHeightMap(heightMap);
+                heightMap.Coordinates = heightMap.Coordinates.Select(p =>
+                {
+                    if (p.Elevation.GetValueOrDefault(0) <= litto3DDataset.NoDataValue)
+                    {
+                        p.Elevation = min;
+                    }
+                    return p;
+                });
+                
+                //=======================
+                // Textures
+                //
+                PBRTexture pbrTexture = null;
+                if (withTexture)
+                {
+                    var bbox4326 = bbox.ReprojectTo(2154, 4326);
+                    Console.WriteLine("Download image tiles...");
+                    TileRange tiles = _imageryService.DownloadTiles(bbox4326, imageryProvider, TEXTURE_TILES);
+                    string fileName = Path.Combine(outputDir, "Texture.jpg");
+
+                    Console.WriteLine("Construct texture...");
+                    TextureInfo texInfo = _imageryService.ConstructTexture(tiles, bbox4326, fileName, TextureImageFormat.image_jpeg);
+
+                    //
+                    //=======================
+
+                    //=======================
+                    // Normal map
+                    Console.WriteLine("Height map...");
+                    //float Z_FACTOR = 0.00002f;
+
+                    //hMap = hMap.CenterOnOrigin().ZScale(Z_FACTOR);
+                    //var normalMap = _imageryService.GenerateNormalMap(heightMap, outputDir);
+
+                    pbrTexture = PBRTexture.Create(texInfo, null);// normalMap);
+
+                    //hMap = hMap.CenterOnOrigin(Z_FACTOR);
+                    //
+                    //=======================
+                }
+                // Triangulate height map
+                // and add base and sides
+                _logger.LogInformation($"Triangulating height map and generating 3D mesh...");
+
+                var model = _sharpGltfService.CreateTerrainMesh(heightMap, pbrTexture);
+                model.SaveGLB(Path.Combine(Directory.GetCurrentDirectory(), modelName + ".glb"));
+                
+
+
+                _logger.LogInformation($"Model exported as {Path.Combine(Directory.GetCurrentDirectory(), modelName + ".gltf")} and .glb");
+
+                //var point = new GeoPoint(43.01142119356318, 6.385200681010872).ReprojectTo(4326, 2154);
+                //point = _elevationService.GetPointElevation(point, litto3DDataset);
+
             }
             catch (Exception e)
             {
                 _logger.LogError(e, e.Message);
             }
+        }
+
+        private DEMDataSet GetLitto3D_1Metre(string datasetPath, bool firstRun)
+        {
+            var dataSetLitto3D = new DEMDataSet()
+            {
+                Name = "Litto3D_PortCros",
+                Description = "Litto3D_PortCros",
+                PublicUrl = null,
+                DataSource = new LocalFileSystem(datasetPath),
+                FileFormat = new DEMFileDefinition("ASCIIGrid", DEMFileType.ASCIIGrid, ".asc", DEMFileRegistrationMode.Cell),
+                ResolutionMeters = 1,
+                SRID = Reprojection.SRID_PROJECTED_LAMBERT_93,
+                Attribution = new Attribution("Dataset", "Litto3D", "www.shom.fr", "Licence ouverte Etalab"),
+                NoDataValue= -99999
+            };
+            if (firstRun)
+            {
+                _rasterService.GenerateDirectoryMetadata(dataSetLitto3D, force: firstRun);
+            }
+            return dataSetLitto3D;
+        }
+        private DEMDataSet GetLitto3D_5Metres(string datasetPath, bool firstRun)
+        {
+            var dataSetLitto3D = new DEMDataSet()
+            {
+                Name = "Litto3D_PortCros5m",
+                Description = "Litto3D_PortCros5m",
+                PublicUrl = null,
+                DataSource = new LocalFileSystem(datasetPath),
+                FileFormat = new DEMFileDefinition("ASCIIGrid", DEMFileType.ASCIIGrid, ".asc", DEMFileRegistrationMode.Cell),
+                ResolutionMeters = 5,
+                SRID = Reprojection.SRID_PROJECTED_LAMBERT_93,
+                Attribution = new Attribution("Dataset", "Litto3D", "www.shom.fr", "Licence ouverte Etalab"),
+                NoDataValue = -99999
+            };
+            if (firstRun)
+            {
+                _rasterService.GenerateDirectoryMetadata(dataSetLitto3D, force: firstRun);
+            }
+            return dataSetLitto3D;
         }
 
 
