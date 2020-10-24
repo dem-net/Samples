40125c2138179d826eca55a44fff5d4b132ef2ec
Xavier Fischer <xavier@fischer.ninja>
2020-10-24 22:17:06 +0200

    before refacto (8,9,a chars have issues with interior rings)

--- DEMNet.Sample/Samples/Misc/Text3DSample.cs
+++ DEMNet.Sample/Samples/Misc/Text3DSample.cs
@@ -6,7 +6,10 @@ using Microsoft.Extensions.Logging;
 using SharpGLTF.Schema2;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.IO;
+using System.Linq;
 using System.Numerics;
 using System.Text;
 
@@ -37,29 +40,49 @@ namespace SampleApp
         }
         public void Run()
         {
+
+
+
+            // DjebelMarra
+            var bbox = new BoundingBox(24.098067346557492, 24.42468219234563, 12.7769822830208, 13.087504129660111);
             var dataset = DEMDataSet.SRTM_GL3;
-            var model = GenerateSampleModel(dataset, withTexture: true);
-            if (model != null)
+
+            string modelName = $"{dataset.Name}_{DateTime.Now:yyyyMMdd_HHmmss}";
+            string outputDir = Directory.GetCurrentDirectory();
+
+            var northText = CreateText("8".ToUpper()).Scale(10)
+                .ToGlTFSpace();
+            var textModel = _sharpGltfService.AddMesh(_sharpGltfService.CreateNewModel(), "Text", northText);
+
+            textModel.SaveGLB(Path.Combine(outputDir, modelName + ".glb"));
+
+
+            var modelAndBbox = GenerateSampleModel(bbox, dataset, withTexture: true);
+            if (modelAndBbox.Model != null)
             {
-                string modelName = $"{dataset.Name}_{DateTime.Now:yyyyMMdd_HHmmss}";
-                string outputDir = Directory.GetCurrentDirectory();
+                var model = modelAndBbox.Model;
 
-
+                // bbox size
+                float arrowSizeFactor = (float)modelAndBbox.ProjectedBbox.Height / 3f;
+                float width = (float)modelAndBbox.ProjectedBbox.Width;
+                float zCenter = (float)modelAndBbox.ProjectedBbox.Center[1];
                 // 
                 float PI = (float)Math.PI;
-                var arrow = _meshService.CreateArrow(20, 500, 25, 100, 10).Transform(System.Numerics.Matrix4x4.CreateRotationX(-PI / 2f));
-
+                var arrow = _meshService.CreateArrow()
+                    .Scale(arrowSizeFactor)
+                    .Translate(new Vector3(-width * 0.6f, 0, zCenter))
+                    .ToGlTFSpace();
                 model = _sharpGltfService.AddMesh(model, "Arrow", arrow);
 
-                //var bar1 = _meshService.CreateCylinder(new Vector3(0, 0, 0), 50, 250, VectorsExtensions.CreateColor(255, 0, 0)).RotateX(PI/2f);
-                var bar2 = _meshService.CreateCylinder(new Vector3(0, 0, 0), 50, 250, VectorsExtensions.CreateColor(0, 255, 0)).RotateY(PI / 2f);
-                //var bar3 = _meshService.CreateCylinder(new Vector3(0, 0, 0), 50, 250, VectorsExtensions.CreateColor(0, 0, 255)).RotateZ(PI/2f);
 
 
-                //model = _sharpGltfService.AddMesh(model, nameof(bar1), bar1);
-                model = _sharpGltfService.AddMesh(model, nameof(bar2), bar2);
-                //model = _sharpGltfService.AddMesh(model, nameof(bar3), bar3);
+                //var northText = CreateText("N").Scale(arrowSizeFactor)
+                //    .Translate(new Vector3(-width * 0.6f, arrowSizeFactor * 1.2f, zCenter))
+                //    .ToGlTFSpace();
 
+                //model = _sharpGltfService.AddMesh(_sharpGltfService.CreateNewModel(), "Text", northText);
+
+                //arrow += _meshService.CreateCylinder(new Vector3(0, 0, 0), 50, 250, VectorsExtensions.CreateColor(0, 255, 0));
 
 
 
@@ -71,7 +94,170 @@ namespace SampleApp
             }
         }
 
-        private ModelRoot GenerateSampleModel(DEMDataSet dataset, bool withTexture = true)
+        public TriangulationList<Vector3> CreateText(string text)
+        {
+
+            using (Bitmap bmp = new Bitmap(400, 400))
+            using (GraphicsPath gp = new GraphicsPath())
+            using (Graphics g = Graphics.FromImage(bmp))
+            using (Font f = new Font("Tahoma", 40f))
+            {
+                g.ScaleTransform(4, 4);
+                gp.AddString(text, f.FontFamily, 0, 40f, new Point(0, 0), StringFormat.GenericDefault);
+                g.DrawPath(Pens.Gray, gp);
+                gp.Flatten(new Matrix(), 0.1f);  // <<== *
+                //g.DrawPath(Pens.DarkSlateBlue, gp);
+                //gp.SetMarkers();
+
+                var globalBounds = gp.GetBounds();
+                var avgCharSize = globalBounds.Width / text.Length;
+
+                using (GraphicsPathIterator gpi = new GraphicsPathIterator(gp))
+                {
+
+                    gpi.Rewind();
+
+                    var triangulation = new TriangulationList<Vector3>();
+                    using (GraphicsPath gsubPath = new GraphicsPath())
+                    {
+                        RectangleF bounds = RectangleF.Empty;
+                        List<Vector3> currentLetterPoints = new List<Vector3>();
+                        List<List<Vector3>> currentLetterPointsInt = new List<List<Vector3>>();
+                        // Read all subpaths and their properties  
+                        for (int i = 0; i < gpi.SubpathCount; i++)
+                        {
+                            bool bClosedCurve;
+                            gpi.NextSubpath(gsubPath, out bClosedCurve);
+
+                            var curBounds = gsubPath.GetBounds();
+                            bool isInside = bounds.Contains(curBounds);
+                            bool isEnclosing = curBounds.Contains(bounds);
+                            bool isIntersecting = curBounds.IntersectsWith(bounds);
+                            var closeness = (curBounds.X + curBounds.Width / 2f) + avgCharSize / 2f - bounds.X;
+                            bool isAnotherChar = closeness < avgCharSize * 0.25f;
+
+                            if (isInside)
+                            {
+                                var points = gsubPath.PathPoints.Select(p => new Vector3(p.X, p.Y, 0));
+                                currentLetterPointsInt.Add(new List<Vector3>(points));
+                            }
+                            else
+                            {
+                                if (currentLetterPoints.Count > 0)
+                                {
+                                    triangulation += _meshService.Tesselate(currentLetterPoints, currentLetterPointsInt);
+                                    currentLetterPoints.Clear();
+                                    currentLetterPointsInt.Clear();
+                                    bounds = curBounds;
+                                }
+                                else if (bounds == RectangleF.Empty)
+                                {
+                                    bounds = curBounds;
+                                }
+                                var points = gsubPath.PathPoints.Select(p => new Vector3(p.X, p.Y, 0));
+                                currentLetterPoints.AddRange(points);
+                               
+
+                            }
+                            gsubPath.Reset();
+
+
+                        }
+                        if (currentLetterPoints.Count > 0)
+                        {
+                            triangulation += _meshService.Tesselate(currentLetterPoints, currentLetterPointsInt);
+                        }
+                    }
+
+                    ////var points = gp.PathPoints.Select(p => new Vector3(p.X, p.Y, 0)).ToList();
+                    ////var triangulation = _meshService.Tesselate(points, null);
+
+                    //// Triangulate wall for each ring
+                    //// (We add floor indices before copying the vertices, they will be duplicated and z shifted later on)
+                    //List<int> numVerticesPerRing = new List<int>();
+                    //numVerticesPerRing.Add(points.Count - 1);
+                    ////numVerticesPerRing.AddRange(building.InteriorRings.Select(r => r.Count - 1));
+                    //triangulation = this.TriangulateRingsWalls(triangulation, numVerticesPerRing);
+
+                    //// Roof
+                    //// Building has real elevations
+
+                    //// Create floor vertices by copying roof vertices and setting their z min elevation (floor or min height)
+                    //var floorVertices = triangulation.Positions.Select(pt => new Vector3(pt.X, pt.Y, -10)).ToList();
+                    //triangulation.Positions.AddRange(floorVertices);
+
+                    return triangulation;
+                }
+                for (int i = 0; i < gp.PathPoints.Length; i++)
+                {
+                    PointF p = gp.PathPoints[i];
+                    g.FillEllipse(Brushes.DarkOrange, p.X - 1, p.Y - 1, 2, 2);
+                }
+            }
+        }
+
+        /// <summary>
+        /// https://stackoverflow.com/questions/4243042/c-sharp-point-in-polygon
+        /// Determines if the given point is inside the polygon
+        /// </summary>
+        /// <param name="polygon">the vertices of polygon</param>
+        /// <param name="testPoint">the given point</param>
+        /// <returns>true if the point is inside the polygon; otherwise, false</returns>
+        public static bool IsPointInPolygon4(PointF[] polygon, PointF testPoint)
+        {
+            bool result = false;
+            int j = polygon.Count() - 1;
+            for (int i = 0; i < polygon.Count(); i++)
+            {
+                if (polygon[i].Y < testPoint.Y && polygon[j].Y >= testPoint.Y || polygon[j].Y < testPoint.Y && polygon[i].Y >= testPoint.Y)
+                {
+                    if (polygon[i].X + (testPoint.Y - polygon[i].Y) / (polygon[j].Y - polygon[i].Y) * (polygon[j].X - polygon[i].X) < testPoint.X)
+                    {
+                        result = !result;
+                    }
+                }
+                j = i;
+            }
+            return result;
+        }
+
+        private TriangulationList<Vector3> TriangulateRingsWalls(TriangulationList<Vector3> triangulation, List<int> numVerticesPerRing)
+        {
+            int offset = numVerticesPerRing.Sum();
+
+            int ringOffset = 0;
+            foreach (var numRingVertices in numVerticesPerRing)
+            {
+                int i = 0;
+                do
+                {
+                    triangulation.Indices.Add(ringOffset + i);
+                    triangulation.Indices.Add(ringOffset + i + offset);
+                    triangulation.Indices.Add(ringOffset + i + 1);
+
+                    triangulation.Indices.Add(ringOffset + i + offset);
+                    triangulation.Indices.Add(ringOffset + i + offset + 1);
+                    triangulation.Indices.Add(ringOffset + i + 1);
+
+                    i++;
+                }
+                while (i < numRingVertices - 1);
+
+                // Connect last vertices to start vertices
+                triangulation.Indices.Add(ringOffset + i);
+                triangulation.Indices.Add(ringOffset + i + offset);
+                triangulation.Indices.Add(ringOffset + 0);
+
+                triangulation.Indices.Add(ringOffset + i + offset);
+                triangulation.Indices.Add(ringOffset + 0 + offset);
+                triangulation.Indices.Add(ringOffset + 0);
+
+                ringOffset += numRingVertices;
+
+            }
+            return triangulation;
+        }
+        private (ModelRoot Model, BoundingBox ProjectedBbox) GenerateSampleModel(BoundingBox bbox, DEMDataSet dataset, bool withTexture = true)
         {
 
             try
@@ -84,9 +270,6 @@ namespace SampleApp
 
                 ImageryProvider provider = ImageryProvider.EsriWorldImagery;// new TileDebugProvider(new GeoPoint(43.5,5.5));
 
-
-                // DjebelMarra
-                var bbox = new BoundingBox(24.098067346557492, 24.42468219234563, 12.7769822830208, 13.087504129660111);
 
                 _logger.LogInformation($"Getting height map data...");
 
@@ -134,13 +317,13 @@ namespace SampleApp
                 _logger.LogInformation($"Triangulating height map and generating 3D mesh...");
 
                 var model = _sharpGltfService.CreateTerrainMesh(heightMap, pbrTexture);
-                return model;
+                return (model, heightMap.BoundingBox);
 
             }
             catch (Exception ex)
             {
                 _logger.LogError(ex, ex.Message);
-                return null;
+                return (null, null);
             }
         }
     }
