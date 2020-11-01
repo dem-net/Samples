273f916eaa838cb61d5fb3a702903de676cc7d61
Xavier Fischer <xavier@fischer.ninja>
2020-11-01 16:44:45 +0100

    updated to adornment service usage

--- DEMNet.Sample/Samples/Misc/Text3DSample.cs
+++ DEMNet.Sample/Samples/Misc/Text3DSample.cs
@@ -1,5 +1,6 @@
 ﻿using DEM.Net.Core;
 using DEM.Net.Core.Imagery;
+using DEM.Net.Core.Model;
 using DEM.Net.Core.Voronoi;
 using DEM.Net.glTF.SharpglTF;
 using Microsoft.Extensions.Logging;
@@ -24,10 +25,12 @@ namespace SampleApp
         private readonly ImageryService _imageryService;
         private readonly SharpGltfService _sharpGltfService;
         private readonly MeshService _meshService;
+        private readonly AdornmentsService _adornmentsService;
 
         public Text3DSample(ILogger<Text3DSample> logger
                 , ElevationService elevationService
                 , MeshService meshService
+                , AdornmentsService adornmentsService
                 , RasterService rasterService
                 , ImageryService imageryService
                 , SharpGltfService sharpGltfService)
@@ -37,6 +40,7 @@ namespace SampleApp
             _rasterService = rasterService;
             _imageryService = imageryService;
             _sharpGltfService = sharpGltfService;
+            _adornmentsService = adornmentsService;
             _meshService = meshService;
         }
         public void Run()
@@ -75,25 +79,26 @@ namespace SampleApp
                 // 
                 float PI = (float)Math.PI;
 
+                Stopwatch swAdornments = Stopwatch.StartNew();
                 // Arrow
                 TriangulationList<Vector3> adornments = _meshService.CreateArrow().ToGlTFSpace()
                     .Scale(arrowSizeFactor)
                     .Translate(new Vector3(-projWidth * 0.55f, 0, zCenter));
 
                 // North text 'N'
-                adornments += CreateText("N", VectorsExtensions.CreateColor(255, 255, 255)).ToGlTFSpace()
+                adornments += _adornmentsService.CreateText("N", VectorsExtensions.CreateColor(255, 255, 255)).ToGlTFSpace()
                            .Scale(projHeight / 200f / 5f)
                            .RotateX(-PI / 2)
                            .Translate(new Vector3(-projWidth * 0.55f, arrowSizeFactor * 1.1f, zCenter));
 
                 // Scale bar
-                var scaleBar = CreateScaleBar(width, projWidth, radius: projHeight / 200f).ToGlTFSpace();
+                var scaleBar = _adornmentsService.CreateScaleBar(width, projWidth, radius: projHeight / 200f).ToGlTFSpace();
                 var scaleBarSize = scaleBar.GetBoundingBox().Height;
                 adornments += scaleBar
                     .RotateZ(PI / 2f)
                     .Translate(new Vector3(projWidth / 2, -projHeight / 2 - projHeight * 0.05f, zCenter));
 
-                var text = CreateText($"{dataset.Attribution.Subject}: {dataset.Attribution.Text}{Environment.NewLine}{ImageryProvider.MapBoxSatellite.Attribution.Subject}: {ImageryProvider.MapBoxSatellite.Attribution.Text}", VectorsExtensions.CreateColor(255, 255, 255)).ToGlTFSpace();
+                var text = _adornmentsService.CreateText($"{dataset.Attribution.Subject}: {dataset.Attribution.Text}{Environment.NewLine}{ImageryProvider.MapBoxSatellite.Attribution.Subject}: {ImageryProvider.MapBoxSatellite.Attribution.Text}", VectorsExtensions.CreateColor(255, 255, 255)).ToGlTFSpace();
                 var scale = ((projWidth - scaleBarSize) * 0.9f) / text.GetBoundingBox().Width;
 
                 text = text.Scale((float)scale)
@@ -104,6 +109,8 @@ namespace SampleApp
 
                 // add adornments
                 model = _sharpGltfService.AddMesh(model, "Adornments", adornments, default(Vector4), doubleSided: true);
+                swAdornments.Stop();
+                _logger.LogInformation($"Adornments generation: {swAdornments.ElapsedMilliseconds:N1} ms");
 
                 // Save model
                 model.SaveGLB(Path.Combine(outputDir, modelName + ".glb"));
@@ -112,185 +119,7 @@ namespace SampleApp
             }
         }
 
-        private TriangulationList<Vector3> CreateScaleBar(float modelSize4326, float modelSizeProjected, float radius = 10f)
-        {
-            int nSteps = 4;
-            ScaleBarInfo scaleInfo = GetScaleBarWidth(modelSize4326, modelSizeProjected, scaleBarSizeRelativeToModel: 0.5f, nSteps);
-
-            Vector3 currentPosition = Vector3.Zero;
-            TriangulationList<Vector3> triangulation = new TriangulationList<Vector3>();
-            for (int i = 0; i < nSteps; i++)
-            {
-                currentPosition.Z = scaleInfo.StepSizeProjected * i;
-
-                triangulation += _meshService.CreateCylinder(currentPosition, radius, scaleInfo.StepSizeProjected
-                        , color: i % 2 == 0 ? VectorsExtensions.CreateColor(0, 0, 0) : VectorsExtensions.CreateColor(255, 255, 255));
-            }
-
-            // scale units (m or km ?)
-            string scaleLabel = (scaleInfo.TotalSize / 1000f > 1) ? $"{scaleInfo.TotalSize / 1000:F0} km" : $"{scaleInfo.TotalSize} m";
-
-            triangulation += CreateText(scaleLabel, color: VectorsExtensions.CreateColor(255, 255, 255))
-                            .Scale(radius / 5)
-                            .RotateY((float)Math.PI / 2)
-                            .RotateZ((float)Math.PI / 2)
-                            .Translate(new Vector3(radius * 5, -radius, scaleInfo.TotalSizeProjected / 2));
-
-            return triangulation;
-        }
-
-        private struct ScaleBarInfo
-        {
-            public float TotalSize;
-            public float TotalSizeProjected;
-            public int NumSteps;
-            public float StepSize;
-            public float StepSizeProjected;
-        }
-        private ScaleBarInfo GetScaleBarWidth(float totalWidth, float modelSizeProjected, float scaleBarSizeRelativeToModel = 0.5f, int nSteps = 4)
-        {
-            // must be divisible by 4
-            float[] smallestScaleStep = { 1, 2, 5, 10, 20, 25, 50, 100, 250, 500, 1000, 2000, 2500, 5000, 10000, 20000, 25000, 50000, 100000, 200000, 500000, 1000000, 2000000, 5000000, 10000000 };
-
-            var scaleBarTotalRequestedSize = totalWidth * scaleBarSizeRelativeToModel;
-            var bestScale = smallestScaleStep.Select(s => new { Step = s, diff = Math.Abs(1 - (scaleBarTotalRequestedSize / (s * nSteps))) })
-                              .OrderBy(s => s.diff)
-                              .First();
-            var scaleBarTotalSize = bestScale.Step * nSteps;
-
-            var projSize = MathHelper.Map(0, totalWidth, 0, modelSizeProjected, scaleBarTotalSize, false);
-            var projStepSize = MathHelper.Map(0, totalWidth, 0, modelSizeProjected, bestScale.Step, false);
-            return new ScaleBarInfo
-            {
-                NumSteps = nSteps,
-                TotalSizeProjected = projSize,
-                StepSizeProjected = projStepSize,
-                StepSize = bestScale.Step,
-                TotalSize = scaleBarTotalSize
-            };
-
-        }
-
-        public TriangulationList<Vector3> CreateText(string text, Vector4 color)
-        {
-            List<Polygon<Vector3>> letterPolygons = GetTextPolygons(text);
-            TriangulationList<Vector3> triangulation = _meshService.Extrude(letterPolygons);
-            triangulation.Colors = triangulation.Positions.Select(p => color).ToList();
-            triangulation = triangulation.CenterOnOrigin();
-            return triangulation;
-        }
-
-        public List<Polygon<Vector3>> GetTextPolygons(string text)
-        {
-            Dictionary<int, Polygon<Vector3>> letterPolygons = new Dictionary<int, Polygon<Vector3>>();
-
-            using (Bitmap bmp = new Bitmap(400, 400))
-            using (GraphicsPath gp = new GraphicsPath())
-            using (Graphics g = Graphics.FromImage(bmp))
-            using (Font f = new Font("Tahoma", 40f))
-            {
-                //g.ScaleTransform(4, 4);
-                gp.AddString(text, f.FontFamily, 0, 40f, new Point(0, 0), StringFormat.GenericDefault);
-                g.DrawPath(Pens.Gray, gp);
-                gp.Flatten(new Matrix(), 0.1f);  // <<== *
-                //g.DrawPath(Pens.DarkSlateBlue, gp);
-                //gp.SetMarkers();
-
-                using (GraphicsPathIterator gpi = new GraphicsPathIterator(gp))
-                {
-
-                    gpi.Rewind();
-
-                    var triangulation = new TriangulationList<Vector3>();
-                    using (GraphicsPath gsubPath = new GraphicsPath())
-                    {
-
-
-                        // Read all subpaths and their properties  
-                        for (int i = 0; i < gpi.SubpathCount; i++)
-                        {
-                            gpi.NextSubpath(gsubPath, out bool bClosedCurve);
-                            Debug.Assert(bClosedCurve, "Unclosed character. That's not possible");
-
-                            var currentRing = gsubPath.PathPoints.Select(p => new Vector3(p.X, p.Y, 0)).ToList();
-
-                            List<int> childs = GetIncludedPolygons(currentRing, letterPolygons);
-                            List<int> parents = GetContainerPolygons(currentRing, letterPolygons);
-                            // contains other polygon ?
-                            if (childs.Any())
-                            {
-                                Polygon<Vector3> newPoly = new Polygon<Vector3>(currentRing);
-                                foreach (var key in childs)
-                                {
-                                    letterPolygons.Remove(key, out var child);
-                                    newPoly.InteriorRings.Add(child.ExteriorRing);
-                                }
-                                letterPolygons.Add(i, newPoly);
-                            }
-                            else if (parents.Any())
-                            {
-                                Debug.Assert(parents.Count == 1);
-                                letterPolygons[parents.First()].InteriorRings.Add(currentRing);
-                            }
-                            else
-                            {
-                                letterPolygons.Add(i, new Polygon<Vector3>(currentRing));
-                            }
-
-                            // triangulation += _meshService.Tesselate(currentLetterPoints, currentLetterPointsInt);
-
-                            gsubPath.Reset();
-                        }
-                    }
-                }
-            }
-
-            return letterPolygons.Values.ToList();
-        }
-
-        private List<int> GetContainerPolygons(List<Vector3> currentPolygon, Dictionary<int, Polygon<Vector3>> polygons)
-        {
-            List<int> parents = polygons.Where(p => IsPointInPolygon(p.Value.ExteriorRing, currentPolygon[0]))
-                                        .Select(p => p.Key)
-                                        .ToList();
-            return parents;
-        }
-
-        private List<int> GetIncludedPolygons(List<Vector3> currentPolygon, Dictionary<int, Polygon<Vector3>> polygons)
-        {
-            List<int> childs = polygons.Where(p => IsPointInPolygon(currentPolygon, p.Value.ExteriorRing[0]))
-                                        .Select(p => p.Key)
-                                        .ToList();
-            return childs;
-        }
-
-
-
-        /// <summary>
-        /// https://stackoverflow.com/questions/4243042/c-sharp-point-in-polygon
-        /// Determines if the given point is inside the polygon. (does not support inner 'holes' rings)
-        /// </summary>
-        /// <param name="polygon">the vertices of polygon</param>
-        /// <param name="testPoint">the given point</param>
-        /// <returns>true if the point is inside the polygon; otherwise, false</returns>
-        private bool IsPointInPolygon(List<Vector3> polygon, Vector3 testPoint)
-        {
-            bool result = false;
-            int j = polygon.Count - 1;
-            for (int i = 0; i < polygon.Count; i++)
-            {
-                if (polygon[i].Y < testPoint.Y && polygon[j].Y >= testPoint.Y || polygon[j].Y < testPoint.Y && polygon[i].Y >= testPoint.Y)
-                {
-                    if (polygon[i].X + (testPoint.Y - polygon[i].Y) / (polygon[j].Y - polygon[i].Y) * (polygon[j].X - polygon[i].X) < testPoint.X)
-                    {
-                        result = !result;
-                    }
-                }
-                j = i;
-            }
-            return result;
-        }
-
+      
         private (ModelRoot Model, double widthMeters, double heightMeters, double averageElevation, BoundingBox projectedBbox) GenerateSampleModel(BoundingBox bbox, DEMDataSet dataset, bool withTexture = true)
         {
 
